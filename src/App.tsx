a6fe41219b8de081577c3eec4a930c600200a5d0
Valentin Karrica <vale26@hotmail.gr>
2022-05-21 13:32:25 +0100

    crate/dashboard from MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Dashboard from "./screens/dashboard/Dashboard";
+import  LandingPage  from "./screens/landingPage/LandingPage";
 
 function App() {
   return (
     <React.Fragment>
-      <div>hello Valentino</div>
+      <Dashboard/>
+    
+      <LandingPage />
     </React.Fragment>
-  )
+  );
 }
 export default App;
---
+++ src/screens/landingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Content = styled.div`
+  background-color: green;
+`;
+
+const LandingPage = () => {
+  return <Content> hello Valentino</Content>;
+};
+
+export default LandingPage;
