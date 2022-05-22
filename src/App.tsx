807783f3b1d3008e9bd55a932e924d76d14b2079
Valentin Karrica <vale26@hotmail.gr>
2022-05-22 01:15:47 +0100

    create/LogIn and SingUp componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Dashboard from "./screens/dashboard/Dashboard";
-import  LandingPage  from "./screens/landingPage/LandingPage";
+import LandingPage from "./screens/landingPage/LandingPage";
 
 function App() {
   return (
     <React.Fragment>
-      <Dashboard/>
-    
       <LandingPage />
     </React.Fragment>
   );
--- src/screens/landingPage/LandingPage.tsx
+++ src/screens/landingPage/LandingPage.tsx
@@ -1,12 +1,40 @@
-import React from "react";
 import styled from "styled-components";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-const Content = styled.div`
-  background-color: green;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 700px;
+  background-image: url("/blueSky.png");
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
+const Content = styled.h1``;
 
 const LandingPage = () => {
-  return <Content> hello Valentino</Content>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Weather App
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">SingUp</Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Content>Welcome on board!</Content>
+      </Container>
+    </Box>
+  );
 };
 
 export default LandingPage;
